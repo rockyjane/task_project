03cee6288f5d0b0ac5436260f1bf3ea65dd5fd33
rockyjane <wooood80@gmail.com>
2026-04-13 15:34:38 +0800

    feat(py): main - 掛載 user 資料表至入口處(可在FastAPI Swagger 預覽)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from routers.user import router as user_router # 匯入 user router
 
-app = FastAPI()
+app = FastAPI() # 建立 app（像 Vue new Vue()）
+app.include_router(user_router) # 掛上 router（像 use router）
 
 @app.get("/")
 def root():
